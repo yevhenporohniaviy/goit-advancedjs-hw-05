0f35330607c8895379abb019ab7d3565fd4d923e
yevhenporohniaviy <porohniaviy.yevheni@gmail.com>
2024-02-11 12:16:31 +0200

    feat:init

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import path from "path";
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    open: true,
+  },
+});
